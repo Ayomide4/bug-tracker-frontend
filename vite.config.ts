46e596bc5e33997368306e653f0e10dfc91a3303
Ayo Omotosho <aomotosho4@gmail.com>
2023-09-06 00:07:48 -0500

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default {
   // ...
   build: {
     rollupOptions: {
-      external: ['react-spinners/ClipLoader'],
+      external: ["react-spinners"],
     },
   },
 };
